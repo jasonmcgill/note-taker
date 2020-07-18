95066886dc71d2f363e600d3df300c18ed86e9a4
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-07-18 14:31:25 -0600

    added working get for index and notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const { db } = require('./db/db');
+const path = require('path');
 
 
+// app.get('/db/db', (req, res) => {
+//     res.json(db);
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
-  });
+});
